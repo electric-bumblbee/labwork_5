63630ed2f316187e1293b68be44b407f0aa62d3c
Anastasia Kochkova <93382533+electric-bumblbee@users.noreply.github.com>
2021-11-27 15:52:08 +0300

    Add files via upload

---
+++ lab5-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title ="Лабораторная работа №5 Задание 3 (＾• ω •＾)";
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine("Точка С находится между точками А и В");
+
+            Console.WriteLine("Введите число А");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число С");
+            int C  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число В");
+            int B = Convert.ToInt32(Console.ReadLine());
+           
+            int AC = Math.Abs(C - A);
+            int BC = Math.Abs(B - C);
+            int l = AC * BC;
+
+            Console.WriteLine($"Произведение отрезков АС и ВС равно {l}");
+
+            Console.ReadKey();
+        }
+    }
+}
